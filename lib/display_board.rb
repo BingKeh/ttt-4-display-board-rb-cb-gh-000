edfc8156f7fd3dbe5da3454c823028d642af9b96
Bin Ge <morhop45@hotmail.com>
2017-03-27 09:19:47 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  i = 0
+  puts " " + board[i] + " | " + board[i + 1] + " | " + board[i + 2] + " "
+  2.times do
+      i += 3
+      puts "-----------"
+      puts " " + board[i] + " | " + board[i + 1] + " | " + board[i + 2] + " "
+  end
+end
